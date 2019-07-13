beac0462686eee6743af1747bcfe200e60a393d0
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-07-13 15:16:33 -0400

    refactor: IIFE pattern to player class

--- js/src/player.js
+++ js/src/player.js
@@ -1,18 +1,24 @@
-let player = (function(){
- //creates a player object
-  let name = name;
-  let playerScore = score;
-  let turn = false;
-
-  function isPlayerTurn(){
-    this.turn = true;
+class Player {
+   //creates a player object
+  constructor(name, number){
+    this.name = name;
+    this.number = number; //player 1 or 2
+    this.score = 0;
+    this.turn = false;
   }
 
-  return{
-    name: name,
-    playerScore: playerScore,
-    isPlayerTurn: isPlayerTurn
+  setTurn(){
+    //changes the turn to this player
+    this.turn = !this.turn;
   }
+}
 
 
-})(name);
+
+  
+
+
+
+
+
+
