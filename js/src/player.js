6549c860e360b0c7796bb5857c90deedfff92776
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-07-14 21:59:43 -0400

    removed the isTurn Function and addd a color attribute to the player class

--- js/src/player.js
+++ js/src/player.js
@@ -1,16 +1,13 @@
 class Player {
    //creates a player object
-  constructor(name, number){
+  constructor(name, number, turn, color){
     this.name = name;
     this.number = number; //player 1 or 2
     this.score = 0;
-    this.turn = false;
+    this.turn = turn;
+    this.color = color;
   }
 
-  setTurn(){
-    //changes the turn to this player
-    this.turn = !this.turn;
-  }
 }
 
 
