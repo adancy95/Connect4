f2f43b546307f9f836cdceaecd74cb2e69770028
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-07-14 22:03:23 -0400

    changed the gamePositions Array and added. Refactored the setCurrentPlayer and getCurrentPlayer Methods. Additionally, Created a findFirstUnfilledCircle Method.

--- js/src/gameState.js
+++ js/src/gameState.js
@@ -1,33 +1,43 @@
 class gameState {
 
-  constructor(){
-    this.gamePositions = [
-      //[R5, R4, R3, R2, R1, R0]
-        [0, 0, 0, 0, 0, 0], //c0
-        [0, 0, 0, 0, 0, 0], //c1
-        [0, 0, 0, 0, 0, 0], //c2
-        [0, 0, 0, 0, 0, 0], //c3
-        [0, 0, 0, 0, 0, 0], //c4
-        [0, 0, 0, 0, 0, 0], //c5
-        [0, 0, 0, 0, 0, 0], //c6
-      
-      ]      
+  constructor(player1, player2){
+    this.gamePositions = [[],[],[],[],[],[],[]];
+    this.player1 = player1;
+    this.player2 = player2; 
+    this.currentPlayer = document.getElementById("current-player")   
   }
 
-  positionsFilled(indexOfColumn, player){
-    //updates an element in an array to fill the position with a player's game peiece
-    let i;
-    let position = gamePositions[indexOfColumn][i];
-    for( i = 0 ; i < 6; i++){
-      if(position === 0){
-        position = player.number;
-      }
+  setCurrentPlayer(){
+    //set the current player in the game
+    if(this.player1.turn){
+      this.player1.turn = false;
+      this.player2.turn = true;
+      this.currentPlayer.innerText = this.player2.number;
+    } else if(this.player2.turn){
+      this.player2.turn = false;
+      this.player1.turn = true;
+      this.currentPlayer.innerText = this.player1.number;
     }
+  }
+
+  getCurrentPlayer(){
+    //returns the current player
+    if(this.player1.turn){
+      return this.player1
+    } else if(this.player2.turn) {
+      return this.player2
+    }
+  }
+
+  findFirstUnfilledCircle(column){
+    //returns the first unfilled circle in the column
+      return [...this.gamePositions[column]].reverse().find(circle => circle.status === 0)
+    
   }
 
   columnFull(column){
     //determines if a column is full
-    if(column.includes(0) === False){
+    if(column.includes(0) === false){
       return true;
     }
   }
