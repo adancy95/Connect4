61e64933e12c675d9329fbc5f1ab2562baa21971
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-07-14 22:00:33 -0400

    added a new player to the game

--- js/src/main.js
+++ js/src/main.js
@@ -1,7 +1,14 @@
 //creates a new game
 let newGame = new gameBoard();
 
-newGame.drawAllGamePositions()
 
 // creates a new player
-let p1 = new Player("Ant", 1);
+let p1 = new Player("Ant", 1, true, "green");
+let p2 = new Player("noah", 2, false, "red");
+
+
+let newGameState = new gameState(p1, p2);
+
+
+newGame.addClickEventToBtns(newGameState);
+newGame.drawAllGamePositions(newGameState);
