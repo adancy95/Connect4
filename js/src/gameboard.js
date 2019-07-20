20fc1e1fd4b56d4d7abe75974f72d9183816ae00
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-07-20 19:07:26 -0400

    added updateAllGamePositions method

--- js/src/gameboard.js
+++ js/src/gameboard.js
@@ -1,52 +1,112 @@
 class gameBoard{
     //creates a Connect Four GameBoard
-    constructor(){
+    constructor( gameStateObj){
       this.ctx = document.getElementById("gameCanvas").getContext("2d");
-      this.btns = document.querySelectorAll(".col-selector > button");
+      this.x = 0;
+      this.y = 0;
+      this.speedX = 0;
+      this.speedY = 0;    
+      this.gravity = 0.05;
+      this.gravitySpeed = 0;
+      this.gameStateObj = gameStateObj;
+      this.color = "white";
+      this.active = true;
+      this.targetY = 0;
     }
 
-    drawCircle(xCord, yCord){
+    update(){
+      this.updateAllGamePositions()
+      this.drawCircle(this.x, this.y, this.color)
+    }
+
+    targetPosition(){
+      if(this.y > 700){
+        console.log(`This.y is ${this.y} and this.target y ${this.targetY}`)
+        this.y = this.targetY
+        this.active = false;
+        this.x = 0;
+        this.y = 0;
+        this.ctx.clearRect(0, 0, 1000,700);
+        this.drawAllGamePositions()
+        this.drawCircle(this.x, this.targetY, this.color)
+      }
+    
+    }
+  
+    newPosition(){
+      this.gravitySpeed += this.gravity;
+      this.x += this.speedX;
+      this.y += this.speedY + this.gravitySpeed;  
+      this.targetPosition()
+    }
+  
+    updateGameArea(){
+      // // DropCircle from the top of column. 
+      // When it reaches the target location
+      // change the color of the target circle to the player's circle
+      this.ctx.clearRect(0, 0, 1000,700);
+      this.newPosition();
+      this.update();
+
+     console.log(this.active)
+      
+      if(this.active){
+        requestAnimationFrame(this.updateGameArea.bind(this));
+      }
+       
+      
+      
+    }
+  
+  
+
+    drawCircle(xCord, yCord, color){
       //draws a circle on a canvas given the x and y coordinate
       this.ctx.beginPath();
       this.ctx.arc(xCord, yCord, 50, 0, 2 * Math.PI);
-      this.ctx.fillStyle = "white";
+      this.ctx.fillStyle = color;
       this.ctx.fill();
       this.ctx.strokeStyle = "brown";
       this.ctx.stroke();
     }
   
-    drawAllGamePositions(gameStateObj){
+    drawAllGamePositions(){
       //draws the 42 game positions on the board by creating evenly spaced circles in a 6 by 7 grid
       let x = 0;
       let y = 0;
       let rowNum = 0;
+      
       for(let i = 0; i < 42; i++){
+       
         if(i % 7 === 0){
           x = 125;
           y+=125;
-          this.drawCircle(x, y);
+          this.drawCircle(x, y, this.color);
           rowNum += 1;
-          gameStateObj.gamePositions[(i % 7)].push(
+          this.gameStateObj.gamePositions[(i % 7)].push(
             {
               circle: i + 1,
               xLoc: x,
               yLoc: y,
               col: (i % 7) ,
-              row: rowNum,
-              status: 0
+              row: rowNum - 1,
+              status: 0,
+              color: this.color
             }
           )
+          
         } else {
           x+=125;
-          this.drawCircle(x, y);
-          gameStateObj.gamePositions[[(i % 7)]].push(
+          this.drawCircle(x, y, this.color);
+          this.gameStateObj.gamePositions[[(i % 7)]].push(
             {
               circle: i + 1,
               xLoc: x,
               yLoc: y,
               col: (i % 7),
-              row: rowNum,
-              status: 0
+              row: rowNum - 1,
+              status: 0,
+              color: this.color
             })
         }
       }
@@ -55,42 +115,43 @@ class gameBoard{
       
     }
 
-    addCheckerToGrid(){
-      //add animation to mimic a player inserting a checker into the grid
+    updateAllGamePositions(){
+      //draws the 42 game positions on the board by creating evenly spaced circles in a 6 by 7 grid
+      let x = 0;
+      let y = 0;
+      for(let i = 0; i < 42; i++){
+        if(i % 7 === 0){
+          x = 125;
+          y+=125;
+          this.drawCircle(x, y, this.gameStateObj.gamePositions.color);
+
+        
+        } else {
+          x+=125;
+          this.drawCircle(x, y, this.gameStateObj.gamePositions.color);
+        
+        }
+      }
+
+      
+      
     }
 
-    addClickEventToBtns(gameStateObj){
-      this.btns.forEach(function(button){
-        button.addEventListener('click', function(e){
-          //change the status of a particular circle on the board from empty to full
-          //associates a player with a circle
-          //find the first empty circle in the column
-          let currentCircleStatus = gameStateObj.findFirstUnfilledCircle(e.target.innerText);
-          currentCircleStatus.status = gameStateObj.getCurrentPlayer().number;
-
-          //add animations to drop the cirlce down the hole.
+   
+      
 
 
-          //switch current player
-          gameStateObj.setCurrentPlayer()
-
-         
-          
-         
-          
-          
     
-        });
-      })
-
-    }
+    
+    
+}
       
   
 
    
 
    
-}
+
 
 
 
